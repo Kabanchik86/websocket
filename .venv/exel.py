0f5b1760511bb3de41ee0e80e83300d687a82690
Anton Kuznetsov <anton66656@yandex.ru>
2026-01-24 17:21:12 +0100

    initial clean commit

---
+++ .venv/exel.py
@@ -0,0 +1,57 @@
+import gspread # Импортируем gspread — библиотека для работы с Google Sheets
+from google.oauth2.service_account import Credentials #Импортируем класс Credentials из Google API Используется для авторизации через файл credentials.json, который ты скачал при создании сервисного аккаунта.
+from datetime import datetime
+
+scopes = ['https://www.googleapis.com/auth/spreadsheets'] #Указываем область доступа (scopes) Это означает: «даю доступ к Google Sheets полностью».
+creds = Credentials.from_service_account_file('credentials.json', scopes=scopes) #Загружаем credentials.json и создаём объект creds #/etc/secrets/credentials.json
+
+client = gspread.authorize(creds) #Авторизуемся в gspread
+sheet_id = '1bUJq4hJAROo9CfcCaorSGJXo1Ecz5UgdmNpRouBBQjY' #Указываем ID твоей таблицы
+workbook = client.open_by_key(sheet_id) #Открываем саму таблицу
+
+sheet1 = workbook.worksheet('отскок')
+sheet2 = workbook.worksheet('Лист2')
+sheet3 = workbook.worksheet('пробой')
+sheet4 = workbook.worksheet('fastbot')
+sheet5 = workbook.worksheet('arbitrage')
+
+def write_to_sheet(coin_name, sol, amount, input_mint):
+    """
+    Записывает данные в первую пустую строку
+    A - coin_name
+    B - sol
+    C - amount
+    D - input_mint
+    """
+
+    # Берём все значения из колонки A
+    col_a = sheet2.col_values(1)
+
+    # Номер первой пустой строки
+    row = len(col_a) + 1
+
+    sheet2.update(
+        f"A{row}:D{row}",
+        [[coin_name, sol, amount, input_mint]]
+    )
+
+
+def write_to_arbitrage(buy, sell, spread, need_base):
+    """
+    Записывает данные в первую пустую строку
+    A - coin_name
+    B - sol
+    C - amount
+    D - input_mint
+    """
+
+    # Берём все значения из колонки A
+    col_a = sheet5.col_values(1)
+
+    # Номер первой пустой строки
+    row = len(col_a) + 1
+
+    sheet5.update(
+        f"A{row}:D{row}",
+        [[buy, sell, spread, need_base]]
+    )
---
+++ .venv/main.py
@@ -0,0 +1,64 @@
+import asyncio, json, time, requests, websockets
+from asyncio.exceptions import CancelledError
+from okx import okx
+from ku_koin import kucoin_ws
+from exel import write_to_arbitrage
+
+prices = {
+    "okx":   {"ask": None, "bid": None, "ask_qty": None, "bid_qty": None, "ts": None},
+    "kucoin":{"ask": None, "bid": None, "ask_qty": None, "bid_qty": None, "ts": None},
+}
+
+async def main():
+    await asyncio.gather(
+        okx(prices),
+        kucoin_ws(prices),
+        compare_loop()
+    )
+
+
+async def compare_loop():
+    USDT_AMOUNT = 10 # 10$
+    MIN_SPREAD = 0.005   # 0.5% для старта
+    TTL_MS = 6000        # котировка считается свежей 8 сек
+
+    while True:
+        okx = prices["okx"]
+        kuc = prices["kucoin"]
+
+        # есть ли обе котировки
+        if okx["ask"] and okx["bid"] and kuc["ask"] and kuc["bid"]:
+            now_ms = int(time.time() * 1000)
+            # print(okx['ts'])
+            # print(now_ms - okx["ts"])
+            # print(TTL_MS)
+            # свежие ли данные
+            if (now_ms - okx["ts"] <= TTL_MS) and (now_ms - kuc["ts"] <= TTL_MS):
+
+                # Направление 1: BUY OKX (ask) -> SELL KuCoin (bid)
+                buy = okx["ask"] # лучшая цена продажи
+                sell = kuc["bid"] # лучшая цена покупки
+                need_base = USDT_AMOUNT / buy
+
+                if okx["ask_qty"] >= need_base and kuc["bid_qty"] >= need_base:
+                    spread = (sell - buy) / buy # спред, разница между биржами
+                    if spread <= MIN_SPREAD:
+                        #print(f"[ARB] BUY OKX @{buy} -> SELL KUCOIN @{sell} | {spread*100:.2f}% | need {need_base:.4f} TON")
+                        write_to_arbitrage(buy, sell, spread, need_base)
+
+                # Направление 2: BUY KuCoin (ask) -> SELL OKX (bid)
+                buy = kuc["ask"]
+                sell = okx["bid"]
+                need_base = USDT_AMOUNT / buy
+
+                if kuc["ask_qty"] >= need_base and okx["bid_qty"] >= need_base:
+                    spread = (sell - buy) / buy
+                    if spread >= MIN_SPREAD:
+                        #print(f"[ARB] BUY OKX @{buy} -> SELL KUCOIN @{sell} | {spread*100:.2f}% | need {need_base:.4f} TON")
+                        write_to_arbitrage(buy, sell, spread, need_base)
+
+
+        await asyncio.sleep(0.2)  # 200 мс
+
+if __name__ == '__main__':
+    asyncio.run(main())

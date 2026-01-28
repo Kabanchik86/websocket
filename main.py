fa8c91abbbe27277b890bace6f70f756a203564c
Anton Kuznetsov <anton66656@yandex.ru>
2026-01-28 12:58:53 +0100

    Commit 10

--- main.py
+++ main.py
@@ -67,6 +67,8 @@ async def compare_loop():
             okx = prices["okx"][pair]
             kuc = prices["kucoin"][pair]
             buy_bit = prices["buy_bit"][pair]
+            print(f'Buy_bit {okx}')
+            print(f'Buy_bit {kuc}')
             print(f'Buy_bit {buy_bit}')
             # есть ли все котировки
             if (okx["ask"] is not None and okx["bid"] is not None
